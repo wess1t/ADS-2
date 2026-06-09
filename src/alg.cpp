716c1362434d5433989d71f3f7754f66f152641f
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-09 12:44:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,54 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.;
+  for (uint64_t i = 0; i < n; i++)
+    res *= val;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint64_t i = 2; i <= n; i++)
+    res *= i;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double s = 0.;
+  for (uint64_t n = 0; n < count; n++)
+    s += calcItem(x, n);
+  return s;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double s = 0.;
+  for (uint64_t n = 1; n <= count; n++) {
+    uint64_t e = 2 * n - 1;
+    double t = pown(x, e) / (double)fact(e);
+    if (n % 2 == 0) {
+      s -= t;
+    } else {
+      s += t;
+    }
+  }
+  return s;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double s = 0.;
+  for (uint64_t n = 1; n <= count; n++) {
+    uint64_t e = 2 * n - 2;
+    double t = pown(x, e) / (double)fact(e);
+    if (n % 2 == 0) {
+      s -= t;
+    } else {
+      s += t;
+    }
+  }
+  return s;
 }
