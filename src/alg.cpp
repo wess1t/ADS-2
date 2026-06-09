8c8a324a2d4b8e13ecbb727c447af1da01a45a23
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-09 13:04:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,54 +4,54 @@
 
 
 double pown(double value, uint16_t n) {
-  double res = 1.;
-  for (uint64_t i = 0; i < n; i++)
-    res *= value;
-  return res;
+    double res = 1.;
+    for (uint16_t i = 0; i < n; i++)
+        res *= value;
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t res = 1;
-  for (uint64_t i = 2; i <= n; i++)
-    res *= i;
-  return res;
+    uint64_t res = 1;
+    for (uint16_t i = 2; i <= n; i++)
+        res *= i;
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / static_cast<double>(fact(n));
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  double s = 0.;
-  for (uint64_t n = 0; n < count; n++)
-    s += calcItem(x, n);
-  return s;
+    double sum = 0.;
+    for (uint16_t n = 0; n <= count; n++)
+        s += calcItem(x, n);
+    return s;
 }
 
 double sinn(double x, uint16_t count) {
-  double s = 0.;
-  for (uint64_t n = 1; n <= count; n++) {
-    uint64_t e = 2 * n - 1;
-    double t = pown(x, e) / static_cast<double>(fact(e));
-    if (n % 2 == 0) {
-      s -= t;
-    } else {
-      s += t;
+    double s = 0.;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t e = 2 * n - 1;
+        double t = pown(x, e) / static_cast<double>(fact(e));
+        if (n % 2 == 0) {
+            s -= t;
+        } else {
+            s += t;
+        }
     }
-  }
-  return s;
+    return s;
 }
 
 double cosn(double x, uint16_t count) {
-  double s = 0.;
-  for (uint64_t n = 1; n <= count; n++) {
-    uint64_t e = 2 * n - 2;
-    double t = pown(x, e) / static_cast<double>(fact(e));
-    if (n % 2 == 0) {
-      s -= t;
-    } else {
-      s += t;
+    double s = 0.;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t e = 2 * n - 2;
+        double t = pown(x, e) / static_cast<double>(fact(e));
+        if (n % 2 == 0) {
+            s -= t;
+        } else {
+            s += t;
+        }
     }
-  }
-  return s;
+    return s;
 }
